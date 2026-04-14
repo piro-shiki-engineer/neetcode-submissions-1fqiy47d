d44e870c2f3d0f49864cc9fa11492e549666f0e1
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-04-15 01:23:54 +0900

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    """
+    Task: Return the maximum profit
+
+    restricts:
+    - we can own only one NeetCoin
+    - If you sell coin, you cant not buy the next day.
+    
+    We have two actions sell and buy
+    if we sell, we need to move less than 2 next day.
+    if we buy, we need to choice keep or sell the coin on the next day.
+
+    Keyword & Idiom:
+    英語：訳
+    technically：厳密には
+
+    TopDown 2DP (caching 2 vars as a key)
+    buyingは、購入できる状態になることをBoolean 表している
+    """
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {} # key: (i, true/bool), value: profit
+        def dfs(i: int, buying: bool) -> int: 
+            if i >= len(prices):
+                return 0
+            if (i, buying) in memo:
+                return memo[(i, buying)]
+            
+            if buying: 
+                buy = dfs(i + 1, not buying) - prices[i]
+                keep = dfs(i + 1, buying)
+                memo[(i, buying)] = max(buy, keep)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                keep = dfs(i + 1, buying)
+                memo[(i, buying)] = max(sell, keep) 
+
+            return memo[(i, buying)]
+        
+        return dfs(0, True)
