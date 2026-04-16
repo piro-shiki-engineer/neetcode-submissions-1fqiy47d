83a91c420f2454bd9c2db390cf876713ad530b34
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-04-17 00:57:40 +0900

    Add: coin-change-ii - submission-4

---
+++ Data Structures & Algorithms/coin-change-ii/submission-4.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+class Solution:
+    """
+    Task: Return the total number of combinations that total up to "amount"
+
+    First Idea: Decsition tree.
+    Let me describe by using whiteboard with example.
+    I want to manage the value
+    node is remaing value from amount to 0
+    
+    TopDown DP
+    DFS: Time: O(len(coins) ^ amount) Space: O(amount)
+    DFS + memo: Time: O(len(coins) * amount) Space: O(amount)
+
+    Keyword & Idiom:   
+    Eng：JP
+    Change the order: 並び順を変える
+    Base Case:終了条件（アルゴリズムの文脈における）
+
+    
+    """
+    def change(self, amount: int, coins: List[int]) -> int:
+        """
+        Bottom Up DP
+        memo
+        key: (start index of coin you can choose, total of coin at this)
+        value: the number of combs that sum up to amount
+        """
+
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = {} 
+
+        def dfs(i, a): # i: the index implify the coins you can select, a is the remain amount 
+            if a == amount:
+                return 1
+
+            if a > amount:
+                return 0
+            
+            if i == len(coins):
+                return 0
+
+            if (i, a) in memo:
+                return memo[(i, a)]
+
+            memo[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            return memo[(i, a)]
+        
+        return dfs(0, 0)
+
+        
