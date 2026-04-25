d9baabe9bfd401c9720635a6abbeb5af283ceba4
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-04-25 13:55:31 +0900

    Add: target-sum - submission-13

---
+++ Data Structures & Algorithms/target-sum/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    Task: Get the different ways that we can build the expression such that the total sum = target
+
+    Top down(DFS)
+    we need to choose adding or substructing each numbers. O(2^n) O(n)
+    we can make this sol efficiently by storing result while using hashmap key is (i, curr_sum)
+
+    """
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        def dfs(i, curr_sum):
+            if i == len(nums):
+                return 1 if curr_sum == target else 0
+
+            if (i, curr_sum) in memo:
+                return memo[(i, curr_sum)]
+
+            memo[(i, curr_sum)] = dfs(i + 1, curr_sum + nums[i]) + dfs(i + 1, curr_sum - nums[i])
+            return memo[(i, curr_sum)]
+
+        return dfs(0, 0)
+        
