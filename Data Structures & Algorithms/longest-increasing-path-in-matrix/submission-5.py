b4eacfabf1173167fe080a5b538d9169e6cc2b74
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-05-04 03:24:11 +0900

    Add: longest-increasing-path-in-matrix - submission-5

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    """
+    Task: Get the maximum length stricly increasing path with matrix.
+
+    We need to manage the result from start which is each sells.
+
+    We do not manage already visited because of the condition which path are consisted by the sell are strictly inreasing.
+    """
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        memo = {}
+        
+        def dfs(r, c, prevVal):
+            if (r < 0 or r == ROWS or
+                c < 0 or c == COLS or
+                matrix[r][c] <= prevVal):
+                return 0
+
+            if (r, c) in memo:
+                return memo[(r, c)]
+
+            best = 1
+            best = max(best, dfs(r + 1, c, matrix[r][c]) + 1)
+            best = max(best, dfs(r - 1, c, matrix[r][c]) + 1)
+            best = max(best, dfs(r, c + 1, matrix[r][c]) + 1)
+            best = max(best, dfs(r, c - 1, matrix[r][c]) + 1)
+
+            memo[(r, c)] = best
+            return best
+        
+        LIP = 1
+        for r in range(ROWS):
+            for c in range(COLS):
+                LIP = max(LIP, dfs(r, c, -1))
+        
+        return LIP
+
