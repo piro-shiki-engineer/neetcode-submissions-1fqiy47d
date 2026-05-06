917292ad9bb84cd56a83faba02a45411f35c7146
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-05-06 20:11:04 +0900

    Add: count-subsequences - submission-10

---
+++ Data Structures & Algorithms/count-subsequences/submission-10.py
@@ -0,0 +1,78 @@
+class Solution:
+    """
+    Task: Return the number of distinct subsequences of s which are equal to t
+
+    英：和
+    distinct: 異なる
+
+    Fisrt: BruteForce
+
+    We want to know the all subsuquences of s.
+    So, I gonna use discition tree.
+    There are 2 options like use or unuse i-th value of s.
+
+    Let me describe some examples for clarifing this sols and this prolbem.
+    """
+    def numDistinct(self, s: str, t: str) -> int:
+        """
+        Time: O(n * m)
+        Space: O(n * m)
+        """        
+        memo = {}
+        def dfs(i, j): # i,j  is index of s, t
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if s[i] == t[j]:
+                memo[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j) # 1項目はiをjと合致する文字として使った時、2項目はiをjと合致する文字として使わなかった時
+            else:
+                memo[(i, j)] = dfs(i + 1, j)
+            
+            return memo[(i, j)]
+                
+        return dfs(0, 0)
+
+    def numDistinct_DFS(self, s: str, t: str) -> int:
+        """
+        Complexity:
+        Time: O(m * 2^n)
+        Space: O(nm) # 各呼び出しで現在の文字列を保持しているため
+
+        n is the length of s. m is the length of t
+        """
+        def dfs(i, curr):            
+            if len(curr) == len(t):
+                return 1 if curr == t else 0
+            
+            if i == len(s):
+                return 0
+            
+            return dfs(i + 1, curr + s[i]) + dfs(i + 1, curr)
+        return dfs(0, "")
+        """
+        Time：O(nm²)（状態数O(nm) × 文字列比較O(m)）
+        Space：O(nm²)（memoのキーの文字列がO(m)）
+
+        これでも奇跡通っているがtで見た時に明らかにprefixとして不正な文字列の結果も保持し、さらに探索している。
+        """
+        memo = {}
+
+        def dfs(i, curr):
+            if (i, curr) in memo:
+                return memo[(i, curr)]
+            
+            if len(curr) == len(t):
+                return 1 if curr == t else 0
+            
+            if i == len(s):
+                return 0
+            
+            memo[(i, curr)] = dfs(i + 1, curr + s[i]) + dfs(i + 1, curr)
+            return memo[(i, curr)]
+        
+        return dfs(0, "")
