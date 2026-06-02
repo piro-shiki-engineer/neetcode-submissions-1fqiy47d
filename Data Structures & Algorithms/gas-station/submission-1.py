8af8506bc8fb0c80a354f7d8219035efa101c2d3
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-06-02 09:29:00 +0900

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    Complexit:
+    time: O(n)
+    space: O(1)
+    """
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        res = 0
+        total = 0
+
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            if total < 0:
+                res = i + 1
+                total = 0
+
+        return res
