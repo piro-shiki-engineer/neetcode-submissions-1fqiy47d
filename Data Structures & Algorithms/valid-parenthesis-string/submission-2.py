d7a67f4aa1a7412eea1a58bb3999064f94a2d598
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-06-23 19:13:41 +0900

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    Task: Check if the input string are met folloing the conditions.
+
+    """
+    def checkValidString(self, s: str) -> bool:
+        minLeft = 0
+        maxLeft = 0
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                minLeft += 1
+                maxLeft += 1
+
+            elif s[i] == ")":
+                minLeft -= 1
+                maxLeft -= 1
+
+            else: # s[i] == "*"
+                minLeft -= 1
+                maxLeft += 1
+        
+            if maxLeft < 0:
+                return False
+            if minLeft < 0:
+                minLeft = 0
+
+        return minLeft == 0
