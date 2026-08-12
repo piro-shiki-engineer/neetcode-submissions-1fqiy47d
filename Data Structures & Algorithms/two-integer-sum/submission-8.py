4f67fe1dc94c94a27f7e3f591c3f077dfcd2a9e1
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-08-13 03:53:07 +0900

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    """
+    Task: Return the indices i and j such that nums[i] + nums[j] == target.
+
+    constrains:
+    - return smaller index first
+    - every input has exactly one pair satify the conditins
+
+    Sol1: BruteForce
+    i to len(nums), j is i + 1 to len(nums) right ?
+
+    Time: O(n^2)
+    Space: O(1)
+
+    Sol2: HashMap
+    nums[j] = target - nums[i]
+
+    for example,  nums = [3, 4, 5, 6] target = 7
+
+    if the index 0 is the index i, nums contains target - nums[0]
+
+    Time: O(n), Space: O(n)
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashMap = {} # key: target - nums[i], value: i 
+
+        for i in range(len(nums)):
+            if nums[i] in hashMap:
+                return [hashMap[nums[i]], i]
+            
+            hashMap[target - nums[i]] = i
+
