c231005bb79d2523b45cb66efb8313d4d878b22a
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-08-14 03:02:25 +0900

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    Task: Grouping words, each elment in a group are anagarms
+
+                                                                                    
+
+    """
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        cntChars = [[0] * 26 ] * len(strs)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            groups[tuple(count)].append(s)
+
+        return list(groups.values())
+        
+
