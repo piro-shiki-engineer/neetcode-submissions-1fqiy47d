f544d5814c28c631592ddb6850c865270d085d2a
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-08-16 03:09:22 +0900

    Add: insert-new-interval - submission-6

---
+++ Data Structures & Algorithms/insert-new-interval/submission-6.py
@@ -0,0 +1,58 @@
+class Solution:
+    """
+    Task: Return inteverls which is merged newInterval into itself.
+
+    - given intervals are sorted by asceding order using start_i
+    - we should merge interval if there are overlappiing intervals
+
+    1. Find the position for inserting newInterval by using Binary Search
+    2. Check if there are orevralapping intervals
+    3. merge the overlapping interval
+
+    """
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        """
+        Greedy Sol
+
+        A greedy approach works because as we scan from left to right, every interval falls into one of three cases relative to newInterval
+        """
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1])
+                ]
+        
+        res.append(newInterval)
+        return res
+
+    def insert_nearly_myidea(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        left, right = 0, len(intervals) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            if intervals[middle][0] > newInterval[0]:
+                right = middle - 1
+            else:
+                left = middle + 1
+        
+        # intervals = intervals[:left] + [newInterval] + intervals[left:]
+        # ↑の方がオーバーヘッドや確保するメモリが大きい
+        intervals.insert(left, newInterval)
+        res = []
+
+        # merge process
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+
+        return res
