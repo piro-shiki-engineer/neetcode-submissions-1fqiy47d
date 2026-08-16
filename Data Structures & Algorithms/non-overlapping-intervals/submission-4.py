c6a2d131476da4440917c2f8cf45f3d456c69671
hiroya.h <105577140+piro-shiki-engineer@users.noreply.github.com>
2026-08-17 02:10:04 +0900

    Add: non-overlapping-intervals - submission-4

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    """
+    Task: Return the minimum number of intervals so that there are no overlapping intervals.
+
+    Edge case:
+    - In this problem, [1, 2] and [2, 3] is not overlapping
+    - In other words, The interval is completely overlapping to the other interval like, [1, 2] and [1, 3]
+    
+    Space: O(1)
+    Time: O(n)
+    n is the length given integers array
+    """
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        
+        prevEnd = intervals[0][1]
+        res = 0
+        for start, end in intervals[1:]:
+            if start >= prevEnd: # In this problem, its ok to have prevEnd and start are same value
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(prevEnd, end)
+        
+        return res
